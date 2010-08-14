9f6af0c0a35b46ae3357c16396da7d022b48131d
Matz Radloff <matzradloff@gmx.de>
2010-08-14 23:09:09 +0200

    ...

--- life.py
+++ life.py
@@ -12,11 +12,43 @@ class Cell:
 		self.posx = posx
 		self.posy = posy
 		self.next_status = False
+		self.nb_left = 
 
 	def check_neighbors():
-		
+		nb_alive = 0
+
+		# Can be improved easily by solving this by a for loop
+
+		if neighbors[self.posx-1, self.posy-1]:
+			nb_alive += 1
+		if neighbors[self.posx, self.posy-1]:
+			nb_alive += 1
+		if neighbors[self.posx+1, self.posy-1]:
+			nb_alive += 1
+		if neighbors[self.posx-1, self.posy+1]:
+			nb_alive += 1
+		if neighbors[self.posx, self.posy+1]:
+			nb_alive += 1
+		if neighbors[self.posx+1, self.posy]:
+			nb_alive += 1
+		if neighbors[self.posx+1, self.posy+1]:
+			nb_alive += 1
+		if neighbors[self.posx-1, self.posy]:
+			nb_alive += 1
+
+
+		if nb_alive < 2:
+			self.next_status = False
+		elif nb_alive > 2 and nb_alive < 4:
+			self.next_status = True
+		elif nb_alive > 4:
+			self.next_status = False
+
 
 	def get_position():
 		return (self.posx, self.posy)
+
+	def aging():
+
 
 	
